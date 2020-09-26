75948cd2d3e6cb0c45be9177b2f3b7076224debc
junknet <junknet@localhost.localdomain>
2020-09-26 16:38:20 +0800

    10 change

--- src/t10_1.rs
+++ src/t10_1.rs
@@ -1,35 +1,33 @@
 pub fn is_match(s: String, p: String) -> bool {
     let p_chars: Vec<char> = p.chars().collect();
-
     let s_chars: Vec<char> = s.chars().collect();
+    let (m, n) = (s.len(), p.len());
     let mut dp: Vec<Vec<bool>> =
         vec![vec![false; p_chars.len() + 1]; s_chars.len() + 1];
     dp[0][0] = true;
-    for i in 0..p_chars.len() {
-        if p_chars[i] == '*' {
-            dp[0][i + 1] = dp[0][i - 1];
-        } else {
-            dp[0][i + 1] = false;
-        }
-    }
-    for i in 1..p_chars.len() + 1 {
-        for j in 1..s_chars.len() + 1 {
-            if p_chars[i - 1] == s_chars[j - 1] || p_chars[i - 1] == '.' {
-                dp[j][i] = dp[j - 1][i - 1];
+    let matches = |i: usize, j: usize| -> bool {
+        if i == 0 {
+            return false;
+        };
+        if p_chars[j - 1] == '.' {
+            return true;
+        };
+        s_chars[i - 1] == p_chars[j - 1]
+    };
+
+    for i in 0..m + 1 {
+        for j in 1..n + 1 {
+            if p_chars[j - 1] == '*' {
+                dp[i][j] |= dp[i][j - 2];
+                if matches(i, j - 1) {
+                    dp[i][j] |= dp[i - 1][j];
+                }
             } else {
-                if p_chars[i - 1] == '*' {
-                    if p_chars[i - 2] == s_chars[j - 1] || p_chars[i - 2] == '.'
-                    {
-                        dp[j][i] = dp[j - 1][i] || dp[j][i - 2];
-                    } else {
-                        dp[j][i] = dp[j][i - 2];
-                    }
-                } else {
-                    continue;
+                if matches(i, j) {
+                    dp[i][j] |= dp[i - 1][j - 1];
                 }
             }
         }
     }
-
-    dp[s_chars.len()][p_chars.len()]
+    dp[m][n]
 }
--- tests/solution.rs
+++
@@ -1,7 +0,0 @@
-use leetcode::*;
-
-#[test]
-
-fn test10() {
-    assert_eq!(t10_1::is_match("qwe".to_string(), "qwe".to_string()), true);
-}
